9d27416dc155ccba66c9d79b8c9b16d0216f09ee
Marcelo <marceloorp@gmail.com>
2012-11-10 12:30:40 -0200

    Testando commit.

--- src/l3_interpreter/SemanticAnalyzer.scala
+++ src/l3_interpreter/SemanticAnalyzer.scala
@@ -16,7 +16,7 @@ case class boolean extends Type
 case class unit extends Type
 case class function(t1: Type, t2: Type) extends Type
 case class tuple(t1: Type, t2: Type) extends Type
-case class record(labels_and_types: (Identifier,Type)*) extends Type
+case class record(labels_and_types: (identifier,Type)*) extends Type
 case class reference(t: Type) extends Type
 
 
@@ -33,15 +33,15 @@ case class referenciation(e: Expr) extends Expr
 case class skip extends Expr
 case class sequence(e1: Expr, e2: Expr) extends Expr
 case class while_do(e1: Expr, e2: Expr) extends Expr
-case class define_function(x: Identifier, t: Type, e: Expr) extends Expr
+case class define_function(x: identifier, t: Type, e: Expr) extends Expr
 case class apply_function(e1: Expr, e2: Expr) extends Expr
-case class Identifier(id: String) extends Expr
-case class let_in_end(x: Identifier, t: Type, e1: Expr, e2: Expr) extends Expr
+case class identifier(id: String) extends Expr
+case class let_in_end(x: identifier, t: Type, e1: Expr, e2: Expr) extends Expr
 //case class let_rec_in_end(f: Identifier, t1: Type, t2: Type, y: Identifier, e1: Expr, e2: Expr) extends Expr
 case class define_tuple(e1: Expr, e2: Expr) extends Expr
 case class project_1(e: Expr) extends Expr
 case class project_2(e: Expr) extends Expr
 case class define_record(labels_and_expressions: (Expr,Type)*) extends Expr
-case class project_label(lab: Identifier, e: Expr) extends Expr
+case class project_label(lab: identifier, e: Expr) extends Expr
 case class raise extends Expr
 case class try_with(e1: Expr, e2: Expr) extends Expr
