0574de47795ff09dfaae020e78c36b758e40fb08
marceloprates <marceloorp@gmail.com>
2012-11-14 12:15:33 -0200

    Implementei um esboço da 'Step()' e um pedaço da 'Substitute()'

--- src/l3_interpreter/SemanticAnalyzer.scala
+++ src/l3_interpreter/SemanticAnalyzer.scala
@@ -13,13 +13,13 @@ object SemanticAnalyzer
       {
         case N(n) => Some(natural())
         case B(b) => Some(boolean())
-        case op(e1,e2,op:plus) =>
+        case operation(e1,e2,op:plus) =>
           (TypeCheck(e1,gamma),TypeCheck(e2,gamma)) match
           {
             case (Some(natural()),Some(natural())) => Some(natural())
             case _ => None
           }
-        case op(e1,e2,op:greaterOrEqual) =>
+        case operation(e1,e2,op:greaterOrEqual) =>
           (TypeCheck(e1,gamma),TypeCheck(e2,gamma)) match
           {
             case (Some(natural()),Some(natural())) => Some(boolean())
@@ -74,14 +74,104 @@ object SemanticAnalyzer
       case _ =>
         e match
         {
-          case op(N(n1),N(n2),op:plus) => Some(N(n1+n2))
-          case op(N(n1),N(n2),op:greaterOrEqual) => Some(B(n1 >= n2))
-          case op(e1,e2,op) =>
-            (Step)
+          case operation(e1,e2,op) =>
+            {
+              (e1,e2,op) match
+              {
+                case (N(n1),N(n2),op:plus) => Some(N(n1+n2))
+                case (N(n1),N(n2),op:greaterOrEqual) => Some(B(n1 >= n2))
+                case (e1,e2,op) =>
+                {
+                  if(e1 IsValue) =>
+                  {
+                    Step(e2) match
+                    {
+                      case Some(e2Lin) => Some(operation(e1,e2Lin,op))
+                      case _ => None
+                    }
+                  }
+                  else
+                  {
+                    Step(e1) match
+                    {
+                      case Some(e1Lin) => Some(operation(e1Lin,e2,op))
+                      case _ => None
+                    }
+                  }
+                }
+                case if_then_else(e1,e2,e3) =>
+                  e1 match
+                  {
+                    case B(true) => e2
+                    case B(false) => e3
+                    case e1 =>
+                      Step(e1) match
+                      {
+                        case Some(e1Lin) => Some(if_then_else(e1Lin,e2,e3))
+                        case _ => None
+                      }
+                    case _ => None
+                  }
+                case apply_function(e1,e2) =>
+                  (e1 IsValue, e2 IsValue) match
+                  {
+                    case (true,true) =>
+                    {
+                      (e1,e2) match
+                      {
+                        case (define_function(x,t,e),v) => Some(Substitute(e,x,v))
+                        case _ => None
+                      }
+                    }
+                    case (false,_) =>
+                    {
+                      Step(e1) match
+                      {
+                        case Some(e1Lin) => Some(apply_function(e1Lin,e2))
+                        case _ => None
+                      }
+                    }
+                    case (true,false) =>
+                    {
+                      Step(e2) match
+                      {
+                        case Some(e2Lin) => Some(apply_function(e1,e2Lin))
+                        case _ => None
+                      }
+                    }
+                  }
+                case let_in_end(x,t,e1,e2) =>
+                {
+                  (e1 IsValue) match
+                  {
+                    case true => Some(Substitute(e2,x,e1))
+                    case false =>
+                    {
+                      Step(e1) match
+                      {
+                        case Some(e1Lin) => Some(let_in_end(x,t,e1Lin,e2))
+                        case _ => None
+                      }
+                    }
+                  }
+                }
+              }
+            }
         }
     }
   }
   */
+
+  def Substitute(e: Expr, x: identifier, v: Expr): Expr = // TO DO
+    {
+      e match
+      {
+        case identifier(id) => v
+        case N(n) => N(n)
+        case B(b) => B(b)
+        case operation(e1,e2,op) => // TO DO
+      }
+    }
 
 }
 
@@ -91,9 +181,19 @@ case class boolean extends Type
 case class function(t1: Type, t2: Type) extends Type
 
 abstract class Expr
+{
+  def IsValue() =
+  {
+    this match
+    {
+      case N(n) | B(b) | function(t1,t2) => true
+      case _ => false
+    }
+  }
+}
 case class N(n: Int) extends Expr
 case class B(b: Boolean) extends Expr
-case class op(e1: Expr, e2: Expr, op: Operator) extends Expr
+case class operation(e1: Expr, e2: Expr, op: Operator) extends Expr
 case class if_then_else(e1: Expr, e2: Expr, e3: Expr) extends Expr
 case class define_function(x: identifier, t: Type, e: Expr) extends Expr
 case class apply_function(e1: Expr, e2: Expr) extends Expr
